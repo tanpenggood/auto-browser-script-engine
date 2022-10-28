7c308d1a311ef0650910fc04243832ab7db69f0d
tanpenggood <tanpengswpu@163.com>
2022-10-29 03:12:08 +0800

    code: finish abstract script of common & root

---
+++ src/main/java/com/itplh/absengine/script/AbstractCommonScript.java
@@ -0,0 +1,73 @@
+package com.itplh.absengine.script;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.itplh.absengine.constant.CommonScriptKeyEnum;
+import com.itplh.absengine.context.Context;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Optional;
+
+@Slf4j
+public abstract class AbstractCommonScript extends AbstractLifeCycleScript {
+
+    @Override
+    public Script populate(String json) {
+        // real time update script
+        realtimeUpdate(this);
+        // hooks, before populate
+        executeHooks(script -> getBeforePopulate());
+
+        this.simplePopulate(json, this);
+        JSONObject jsonObject = JSON.parseObject(json);
+        // set loop
+        Long loop = jsonObject.getLong(CommonScriptKeyEnum.重复执行次数.getValue());
+        loop = loop == null ? 1 : loop;
+        loop = loop < 1 || loop > 999_999_999L ? 1 : loop;
+        this.setLoop(loop);
+
+        doPopulate(jsonObject);
+
+        // hooks, after populate
+        executeHooks(script -> getAfterPopulate());
+
+        return this;
+    }
+
+    @Override
+    public void execute() {
+        // real time update script
+        realtimeUpdate(this);
+
+        log.trace("Line.{} loop:{} {} {}", this.getId(), this.getLoop(), this.getDelayVariable(), this);
+
+        for (int i = 0; i < this.getLoop(); i++) {
+            Context context = getContext();
+            // hooks, before execute
+            executeHooks(script -> getBeforeExecute());
+            // do execute
+            Result result = Result.error();
+            try {
+                result = doExecute(context);
+                log.debug("Line.{} success:{} has element:{} loop:{} {}",
+                        this.getId(), result.isSuccess(), result.hasElement(),
+                        this.getLoop(), this.getDelayVariable());
+            } catch (Exception e) {
+                log.error("{}", e.getMessage());
+            }
+            // fast fail
+            if (result.isError()) {
+                break;
+            }
+            // real time update element
+            Optional.ofNullable(result.getElement()).ifPresent(this::realtimeUpdate);
+            // hooks, after execute
+            executeHooks(script -> getAfterExecute());
+        }
+    }
+
+    public abstract Script doPopulate(JSONObject jsonObject);
+
+    public abstract Result doExecute(Context context);
+
+}
---
+++ src/main/java/com/itplh/absengine/script/AbstractRootScript.java
@@ -0,0 +1,99 @@
+package com.itplh.absengine.script;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.itplh.absengine.annotation.NotNull;
+import com.itplh.absengine.constant.CommonScriptKeyEnum;
+import com.itplh.absengine.constant.ScriptTypeEnum;
+import com.itplh.absengine.context.Context;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.LongAdder;
+
+@Data
+@Slf4j
+public abstract class AbstractRootScript extends AbstractLifeCycleRootScript {
+
+    /**
+     * 循环执行脚本列表的次数
+     * <p>
+     * 默认值为1，0表示无限循环
+     */
+    @NotNull
+    private long globalLoop = 1L;
+
+    private LongAdder load = new LongAdder();
+    private LongAdder execute = new LongAdder();
+
+    @Override
+    public Script populate(String json) {
+        // real time update script
+        realtimeUpdate(this);
+        // hooks, before root populate
+        executeHooks(script -> getBeforeRootPopulate());
+
+        JSONObject jsonObject = JSON.parseObject(json);
+        this.setId(1);
+        this.setScriptTypeEnum(ScriptTypeEnum.ROOT);
+        this.simplePopulate(json, this);
+        // set globalLoop
+        Long loop = jsonObject.getLong(CommonScriptKeyEnum.循环次数.getValue());
+        loop = loop == null || loop < 0 ? 1 : loop;
+        this.setGlobalLoop(loop);
+
+        // hooks, after root populate
+        executeHooks(script -> getAfterRootPopulate());
+
+        return this;
+    }
+
+    @Override
+    public void execute() {
+        // check empty script
+        if (this.getChild() == null || this.getChild().isEmpty()) {
+            return;
+        }
+        // check repeat execute script
+        if (this.getExecute().intValue() > 0) {
+            return;
+        }
+        this.getExecute().increment();
+
+        Context context = getContext();
+        // real time update script
+        realtimeUpdate(this);
+        // hooks, before root execute
+        executeHooks(script -> getBeforeRootExecute());
+        log.info("{} {} {}", this.getScriptName(), this.getGlobalLoop(), this.getDelayVariable());
+
+        for (long i = 0; i < this.getGlobalLoop() || isForeverLoop(); i++) {
+            long start = System.currentTimeMillis();
+            // real time update script
+            realtimeUpdate(this);
+            // check terminal execute
+            if (getTerminalExecute().test(context)) {
+                break;
+            }
+            // hooks, before sub execute
+            executeHooks(script -> getBeforeSubExecute());
+            // execute sub script list
+            this.getChild().forEach(Script::execute);
+            // hooks, after sub execute
+            executeHooks(script -> getAfterSubExecute());
+            log.info("No.{} done, cost:{}ms {} {}", (i + 1), (System.currentTimeMillis() - start),
+                    this.getScriptName(), this.getGlobalLoop());
+        }
+
+        // hooks, after root execute
+        executeHooks(script -> getAfterRootExecute());
+        // close resource
+        remove();
+        log.info("{} execute finish. {}", this.getScriptName(), context.baseUri());
+    }
+
+    private boolean isForeverLoop() {
+        return globalLoop == 0L;
+    }
+
+}
