9fd5c4873ce63d6e4155114142d0322e550e728b
tanpenggood <tanpengswpu@163.com>
2022-10-29 16:25:59 +0800

    optimize(ScriptEngine.java): optimize api

--- src/main/java/com/itplh/absengine/ScriptEngine.java
+++ src/main/java/com/itplh/absengine/ScriptEngine.java
@@ -1,12 +1,22 @@
 package com.itplh.absengine;
 
+import com.itplh.absengine.context.Context;
 import com.itplh.absengine.script.Script;
 import com.itplh.absengine.script.impl.RootScript;
+import com.itplh.absengine.util.HttpUtils;
+import org.jsoup.nodes.Document;
 
 public class ScriptEngine {
 
-    public static RootScript buildRootScript(String scriptName) {
+    public static RootScript buildRoot(String scriptName) {
         return RootScript.getInstance(scriptName);
+    }
+
+    public static RootScript buildRoot(String scriptName, String url) {
+        RootScript root = RootScript.getInstance(scriptName);
+        Document document = HttpUtils.requestGet(url, null).orElse(null);
+        Context.initContext(root, document);
+        return root;
     }
 
     public static Script load(Script root) {
