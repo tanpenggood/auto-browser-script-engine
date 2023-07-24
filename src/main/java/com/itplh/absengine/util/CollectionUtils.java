9eff8842a2fb2332ed16497bbbe2c03fce69cafb
tanpenggood <tanpengswpu@163.com>
2023-07-24 09:01:04 +0800

    feat(CollectionUtils.java): add isEmpty/isNotEmpty method for Map

--- src/main/java/com/itplh/absengine/util/CollectionUtils.java
+++ src/main/java/com/itplh/absengine/util/CollectionUtils.java
@@ -1,15 +1,24 @@
 package com.itplh.absengine.util;
 
 import java.util.Collection;
+import java.util.Map;
 
 public class CollectionUtils {
 
     public static boolean isEmpty(Collection collection) {
-        return collection == null || collection.isEmpty();
+        return collection == null || collection.size() == 0;
     }
 
     public static boolean isNotEmpty(Collection collection) {
         return !isEmpty(collection);
+    }
+
+    public static boolean isEmpty(Map map) {
+        return map == null || map.size() == 0;
+    }
+
+    public static boolean isNotEmpty(Map map) {
+        return !isEmpty(map);
     }
 
 }
